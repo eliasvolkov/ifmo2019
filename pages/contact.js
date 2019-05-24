fa0ad608214d3fa48a5088d2a20fe157e4ff075a
Ilia Volkov <ilya.j0b@yandex.ru>
2019-05-24 13:55:55 +0300

    init pages

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const contact = () => {
+  return (
+    <div>
+      <Layout title="Contact" />
+    </div>
+  );
+};
+
+export default contact;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const index = () => {
+  return (
+    <div>
+      <Layout title="Home">
+        <p>Lorem ipsum dolor sit amet.</p>
+      </Layout>
+    </div>
+  );
+};
+
+export default index;
