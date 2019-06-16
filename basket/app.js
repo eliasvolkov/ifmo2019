1d6971824e3d2e13a9cc1a616211c768b863c99d
Ilia Volkov <ilya.j0b@yandex.ru>
2019-06-17 01:29:41 +0300

    add form and change product's output

--- basket/app.js
+++ basket/app.js
@@ -3,25 +3,35 @@ const cards = document.querySelector(".cards");
 document.addEventListener("DOMContentLoaded", function() {
   const ui = new UI();
   const data = new Data();
-  let totalPrice = 0;
-  let salary = 300;
-  let restPay = salary;
+  let totalPrice = 0,
+    salary,
+    restPay,
+    products = {};
   ui.preLoader();
+
+  document.querySelector("form").addEventListener("submit", function(e) {
+    salary = document.querySelector("#exampleInput").value;
+    restPay = salary;
+    ui.renderSalary(restPay);
+    document.querySelector(".btn").classList.add("disabled");
+    e.preventDefault();
+  });
 
   data.fetchCards().then(cards => {
     ui.renderCards(cards);
 
     document.querySelector(".cards").addEventListener("click", function(e) {
       if (e.target.classList.contains("btn")) {
-        let price = e.target.parentElement.getAttribute("data-price");
-        let title = e.target.parentElement.getAttribute("data-title");
+        let price = e.target.parentElement.getAttribute("data-price"),
+          title = e.target.parentElement.getAttribute("data-title");
 
         totalPrice += Number(price);
         restPay -= Number(price);
+        products[title] = products[title] + 1 || 1;
 
         if (totalPrice <= salary) {
-          ui.renderProducts(title);
           ui.renderPrice(totalPrice);
+          ui.renderProducts(products);
           ui.renderSalary(restPay);
         } else {
           ui.showAlert();
